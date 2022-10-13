bf152d5be538804effb7e3810be84b7953d2e361
Christoffer Frost-Jensen Johansen <s183813@student.dtu.dk>
2022-10-13 14:50:21 +0200

    Final Setup DONE :)

--- Non_Coop/Non_Coop/Non_Coop.cpp
+++ Non_Coop/Non_Coop/Non_Coop.cpp
@@ -1,15 +1,31 @@
 // Non_Coop.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
+// opencv_helloworld.cpp : Defines the entry point for the console 
+//application.
+#include <stdio.h> 
+#include <iostream> 
+#include<opencv2/opencv.hpp> 
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-
-
-
-
+	Mat output;
+	VideoCapture cap(0); //Try changing to 1 if not working 
+	//Check if camera is available 
+	if (!cap.isOpened())
+	{
+		cout << "Could not initialize capturing...\n";
+		return 0;
+	}
+	//Show Camera output 
+	while (1) {
+		cap >> output;
+		imshow("webcam input", output);
+		char c = (char)waitKey(10);
+		if (c == 27) break; //Press escape to stop program 
+	}
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
